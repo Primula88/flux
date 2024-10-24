ffed34fc405e1f1a8da06a5691c5aaff0602c96d
Primula88 <prim@Pr1mula.nl>
2024-10-24 04:39:20 +0200

    push

--- src/components/Header.js
+++ src/components/Header.js
@@ -101,14 +101,6 @@ const Wrapper = styled.section`
   }
 `;
 
-const TrianglesWrapper = styled.div`
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-`;
 
 const ContentWrapper = styled.div`
   position: relative;
